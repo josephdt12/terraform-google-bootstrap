468f4567f1bae0700474616969b86cf128eec158
Joseph Thomas <josephdthomas@google.com>
2024-03-13 20:33:17 +0000

    Adjust example to point to a simple repo

--- examples/im_cloudbuild_workspace_github/main.tf
+++ examples/im_cloudbuild_workspace_github/main.tf
@@ -22,45 +22,11 @@ module "im_workspace" {
 
   tf_repo_type           = "GITHUB"
   im_deployment_repo_uri = var.repository_url
-  im_deployment_ref      = "test-fix-detect-deployment"
-  infra_manager_sa_roles = ["roles/compute.networkAdmin"]
+  im_deployment_ref      = "main"
   tf_cloudbuilder        = "hashicorp/terraform:1.2.3"
 
   // Found in the URL of your Cloud Build GitHub app configuration settings
   // https://cloud.google.com/build/docs/automating-builds/github/connect-repo-github?generation=2nd-gen#connecting_a_github_host_programmatically
-  github_app_installation_id = "47590865"
-
+  github_app_installation_id   = "47236181"
   github_personal_access_token = var.im_github_pat
-}
-
-module "test-vpc-module" {
-  source       = "terraform-google-modules/network/google"
-  version      = "2.6.0"
-  project_id   = var.project_id
-  network_name = "my-example-custom-network"
-
-  subnets = [
-    {
-      subnet_name   = "example-subnet-01"
-      subnet_ip     = "10.10.10.0/24"
-      subnet_region = "us-west1"
-    },
-    {
-      subnet_name           = "example-subnet-02"
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = "us-west1"
-      subnet_private_access = "true"
-      subnet_flow_logs      = "true"
-    },
-    {
-      subnet_name               = "example-subnet-03"
-      subnet_ip                 = "10.10.30.0/24"
-      subnet_region             = "us-west1"
-      subnet_flow_logs          = "true"
-      subnet_flow_logs_interval = "INTERVAL_10_MIN"
-      subnet_flow_logs_sampling = 0.7
-      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-      subnet_flow_logs_filter   = "false"
-    }
-  ]
 }
